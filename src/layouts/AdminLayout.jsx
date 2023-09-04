0e9eff2653d625dce1f8157a74ff4e1d323826ce
David Diez <daviid10paz@gmail.com>
2023-09-04 11:25:03 +0200

    ajuste integrando el toast para que aparezcan en el admin

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,18 +1,24 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "../components/AdminSidebar";
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 import { useAuth } from "../hooks/useAuth";
 
 export default function AdminLayout() {
 
     useAuth({ middleware: 'admin' });
-    
-    return (
-        <div className="md:flex">
-            <AdminSidebar />
 
-            <main className="flex-1 h-screen overflow-y-scroll bg-gray-100 p-3">
-                <Outlet />
-            </main>
-        </div>
+    return (
+        <>
+            <div className="md:flex">
+                <AdminSidebar />
+
+                <main className="flex-1 h-screen overflow-y-scroll bg-gray-100 p-3">
+                    <Outlet />
+                </main>
+            </div>
+
+            <ToastContainer />
+        </>
     )
 }
